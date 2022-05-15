db410467cbd7383a43bab2feb84a82166c55e767
FloWambui <gflorencewambui@gmail.com>
2022-05-15 22:42:24 +0300

    add the user class

--- app/models.py
+++ app/models.py
@@ -1,10 +1,22 @@
+from . import db
+
 class Quote:
     '''
-    Quote class to define Movie Objects
+    Quote class to define Quote Objects
     '''
 
     def __init__(self,id,author,quote):
         self.id =id
         self.author = author
         self.quote = quote
+
+
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer,primary_key = True)
+    username = db.Column(db.String(255))
+
+    def __repr__(self):
+        return f'User {self.username}'
+
     
