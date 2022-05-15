2b174a9cf28951f1d237ac3b5c89469c54632533
FloWambui <gflorencewambui@gmail.com>
2022-05-15 20:53:08 +0300

    update the file structure

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ class Config:
     '''
     General configuration parent class
     '''
-    QOUTE_API_BASE_URL = 'http://quotes.stormconsultancy.co.uk/random.json'
+    
     SECRET_KEY = os.environ.get('SECRET_KEY')
 
 
